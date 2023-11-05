f30dcff0c49d3da6680a74010cc0798651139802
jeelpatel231 <jeelpatel231@gmail.com>
2023-11-05 22:01:23 +0530

    use material colors on drawable tint

--- app/src/main/java/tel/jeelpa/otter/ui/adapters/RelationsAdapter.kt
+++ app/src/main/java/tel/jeelpa/otter/ui/adapters/RelationsAdapter.kt
@@ -1,7 +1,12 @@
 package tel.jeelpa.otter.ui.adapters
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.DrawableCompat
 import coil.load
 import coil.size.Scale
+import com.google.android.material.color.DynamicColors
 import tel.jeelpa.otter.R
 import tel.jeelpa.otter.databinding.MediaRelationSmallLayoutBinding
 import tel.jeelpa.otter.ui.generic.GenericRecyclerAdapter
@@ -14,12 +19,25 @@ class RelationsAdapter(
     private val onItemClick : (MediaCardData) -> Unit
 ): GenericRecyclerAdapter<MediaRelationCardData, MediaRelationSmallLayoutBinding>(MediaRelationSmallLayoutBinding::inflate) {
 
-    private fun getRelationDrawable(relation: MediaRelationType): Int {
-        return when(relation){
+    private fun getRelationDrawable(context: Context, relation: MediaRelationType): Drawable {
+        // get the material dynamic color in kotlin
+        val ta = DynamicColors.wrapContextIfAvailable(context)
+                .obtainStyledAttributes(intArrayOf(com.google.android.material.R.attr.colorOnBackground))
+        val tintColor = ta.getColor(0, 0)
+        ta.recycle()
+
+        val idRes = when(relation){
             // TODO: switch to enum and add more cases
             MediaRelationType.SOURCE -> R.drawable.round_menu_book_24
             else -> R.drawable.round_star_24
         }
+
+        // tint it with the color and return
+        return AppCompatResources.getDrawable(context, idRes)!!.apply {
+            setTint(tintColor)
+            DrawableCompat.setTint(this, tintColor)
+        }
+
     }
 
     override fun onBind(binding: MediaRelationSmallLayoutBinding, entry: MediaRelationCardData, position: Int) {
@@ -30,7 +48,7 @@ class RelationsAdapter(
             scale(Scale.FILL)
         }
 
-        binding.relationIcon.load(getRelationDrawable(entry.relation))
+        binding.relationIcon.load(getRelationDrawable(binding.root.context, entry.relation))
         binding.relationHolder.text = entry.relation.value
 
         binding.score.text = entry.meanScore.toString()
