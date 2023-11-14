bb80b1abc640b0ac0aca44c1ee8b3cafb71ac19c
jeelpatel231 <jeelpatel231@gmail.com>
2023-11-14 15:52:52 +0530

    remove code from local repo, redundant code

--- plugin/src/main/java/Metadata.kt
+++ plugin/src/main/java/Metadata.kt
@@ -1,16 +1,8 @@
 
 import okhttp3.OkHttpClient
 import tel.jeelpa.otter.reference.RegisterUseCase
-import tel.jeelpa.plugin.extractors.AllAnimeDirectExtractor
-import tel.jeelpa.plugin.extractors.AllAnimeExtractor
-import tel.jeelpa.plugin.extractors.AnimePaheExtractor
 import tel.jeelpa.plugin.extractors.DummyExtractor
-import tel.jeelpa.plugin.extractors.StreamTape
-import tel.jeelpa.plugin.extractors.VidStreaming
-import tel.jeelpa.plugin.parsers.AllAnime
-import tel.jeelpa.plugin.parsers.AnimePahe
 import tel.jeelpa.plugin.parsers.DummyParser
-import tel.jeelpa.plugin.parsers.Kaido
 
 class Metadata(
     okHttpClient: OkHttpClient,
@@ -20,19 +12,11 @@ class Metadata(
         // register your parsers like this
         registerUseCase.registerParser(
             DummyParser(),
-            AnimePahe(okHttpClient),
-            Kaido(okHttpClient),
-            AllAnime(okHttpClient)
         )
 
         // register your extractors here
         registerUseCase.registerExtractor(
             DummyExtractor(),
-            StreamTape(okHttpClient),
-            VidStreaming(okHttpClient),
-            AnimePaheExtractor(okHttpClient),
-            AllAnimeExtractor(okHttpClient),
-            AllAnimeDirectExtractor(),
         )
     }
 }
