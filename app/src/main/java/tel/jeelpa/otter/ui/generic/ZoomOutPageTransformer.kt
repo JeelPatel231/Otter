92e73d4d09b2c7e55b6330494f7960662f3e2aae
jeelpatel231 <jeelpatel231@gmail.com>
2023-11-14 12:56:13 +0530

    make linear Y translation so the view wont get get cut off from the top

--- app/src/main/java/tel/jeelpa/otter/ui/generic/ZoomOutPageTransformer.kt
+++ app/src/main/java/tel/jeelpa/otter/ui/generic/ZoomOutPageTransformer.kt
@@ -6,19 +6,22 @@ import androidx.viewpager2.widget.ViewPager2
 
 class ZoomOutPageTransformer : ViewPager2.PageTransformer {
     override fun transformPage(page: View, position: Float) {
+        val scale = 1.1F
         page.apply {
+            translationY = (scale - 1) * page.height / 2
             alpha = 0f
-            scaleX = 1.1F
-            scaleY = 1.1F
+            scaleX = scale
+            scaleY = scale
             visibility = View.VISIBLE
         }
 
         // Start Animation for a short period of time
         page.animate()
+            .translationY(0F)
             .scaleX(1F)
             .scaleY(1F)
             .setDuration(page.resources.getInteger(android.R.integer.config_shortAnimTime).toLong())
-            .setInterpolator(OvershootInterpolator(1.5F))
+            .setInterpolator(OvershootInterpolator(1.3F))
             .alpha(1f)
     }
 }
